052a9fa3275460df16598ee966aa88f446b88957
Jose Maranan <jmmaranan@gmail.com>
2021-08-28 19:30:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Sveltestrap Starter
+
+Combined Bootstrap's NPM Starter, Svelte's template and Sveltestrap components.
+
+Setup:
+
+```
+npm install 
+```
+
+Run Svelte with watch:
+
+```
+npm run dev
+```
+
+Open a _new terminal_ then run Bootstrap with watch:
+
+```
+npm run css-watch
+```
+
+Edit styles:
+`scss/global.scss`
+
+Edit svelte:
+`src/App.svelte`
+
+Production build:
+
+```
+npm run build
+```
---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import {
+    Collapse,
+    Container,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+</script>
+
+<header>
+    <Navbar color="primary" dark expand="md" class="m-auto">
+      <NavbarBrand href="/">Svelte + Bootstrap 5</NavbarBrand>
+      <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+      <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto h5" navbar>
+          <NavItem class="me-2">
+            <NavLink href="https://github.com/jmmaranan/sveltestrap-starter">Github</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+</header>
+
